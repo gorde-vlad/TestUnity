97da24598d0ebaeead41cbf12b88545fa4c5e54b
vlad <gorde-vlad@yandex.ru>
2019-03-21 11:20:37 +0400

    Добавлен BuildSetting

---
+++ Assets/Editor/BuildSetting.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+//using UnityEditor.Build.Reporting;
+
+// Output the build size or a failure depending on BuildPlayer.
+
+public class BuildPlayerExample : MonoBehaviour
+{
+    [MenuItem("Build/Build WebGL")]
+    public static void MyBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scene1.unity"};
+        buildPlayerOptions.locationPathName = "WebGLBuild";
+        buildPlayerOptions.target = BuildTarget.WebGL;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        //BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        //BuildSummary summary = report.summary;
+
+        //if (summary.result == BuildResult.Succeeded)
+        //{
+        //    Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        //}
+
+        //if (summary.result == BuildResult.Failed)
+        //{
+        //    Debug.Log("Build failed");
+        //}
+    }
+}
